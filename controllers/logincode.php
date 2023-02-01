8005799ef5d41352c2dca4b344da12ab1ea5a174
patipan-patisampita <patipan.patisampita@gmail.com>
2023-02-01 17:18:19 +0700

    update

--- controllers/logincode.php
+++ controllers/logincode.php
@@ -3,11 +3,12 @@
 
 <?php
 if(isset($_POST['login_btn'])){
-  echo $email =  mysqli_real_escape_string($con,$_POST['email']);
-  echo $password =  mysqli_real_escape_string($con,$_POST['password']);
+   $email =  mysqli_real_escape_string($con,$_POST['email']);
+   $password =  mysqli_real_escape_string($con,$_POST['password']);
 
   $login_query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
   $login_query_run = mysqli_query($con,$login_query);
+  print_r($login_query_run);
     
 
 }else{
