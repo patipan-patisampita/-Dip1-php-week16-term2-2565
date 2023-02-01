75f0cf994b2e12f26ab21a9b1a90c17587a86440
patipan-patisampita <patipan.patisampita@gmail.com>
2023-02-01 16:28:41 +0700

    update

--- message.php
+++ message.php
@@ -1,6 +1,14 @@
 <?php 
 if(isset($_SESSION['message'])){
-    print($_SESSION['message']);
+    
+?>
+
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong><?php print('<h4>'.$_SESSION['message'].'</h4>'); ?></strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+<?php
     unset($_SESSION['message']);
 }
 ?>
